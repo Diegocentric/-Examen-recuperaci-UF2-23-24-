a5c28686b0890aa312383b1ef99d27d59c038f13
Toni798 <diegolopezblasco7@gmail.com>
2024-06-05 04:51:23 +0200

    Creación y mejora Brand y Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,9 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'price', 'brand_id'];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
 }
